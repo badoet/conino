344004e25e7a705a0a9f985724d29c03bc521312
Donald Wu <yeukfei02@gmail.com>
2018-06-23 16:37:17 +0800

    update

--- frontend/src/components/addPrescriptions/AddPrescriptions.js
+++ frontend/src/components/addPrescriptions/AddPrescriptions.js
@@ -7,57 +7,50 @@ import '../../styles/select2.min.css';
 import Select2 from 'react-select2-wrapper';
 
 class AddPrescriptions extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
 
-
-
-
-    static contextTypes = {
-        router: PropTypes.object
+  constructor() {
+    super();
+    this.state = {
+      drugList: [
+        { text: 'bug', id: 1 },
+        { text: 'feature', id: 2 },
+        { text: 'documents', id: 3 },
+        { text: 'discussion', id: 4 }
+      ]
     };
+  }
+
+  render() {
+    return (
+        <div>
+            <div className="tabTitle">Consultation</div>
+
+            <div className="form-group">
+                <label>Symptoms</label>
+                <textarea className="form-control" id="symptomsTextArea" rows="3"></textarea>
+            </div>
+
+            <div className="form-group">
+                <label>Diagnosis</label>
+                <textarea className="form-control" id="diagnosisTextArea" rows="3"></textarea>
+            </div>
 
 
-    constructor() {
-        super();
-        this.state = {drugList:
-                [
-                { text: 'bug', id: 1 },
-                { text: 'feature', id: 2 },
-                { text: 'documents', id: 3 },
-                { text: 'discussion', id: 4 }
-                ]
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="tabTitle">Consultation</div>
-
-                <div class="form-group">
-                    <label>Symptoms</label>
-                    <textarea class="form-control" id="symptomsTextArea" rows="3"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label>Diagnosis</label>
-                    <textarea class="form-control" id="diagnosisTextArea" rows="3"></textarea>
-                </div>
-
-
-                <div class="form-group">
-                    <label>Prescriptions</label>
-                <Select2
-                    data= {this.state.drugList}
-                    options={{placeholder: 'search by tags',
-                  }}/>
-
-                </div>
+            <div className="form-group">
+                <label>Prescriptions</label>
+            <Select2
+                data= {this.state.drugList}
+                options={{placeholder: 'search by tags',
+              }}/>
 
             </div>
-        );
-    }
 
-;
+        </div>
+    );
+  }
 }
 
 function mapStateToProps() {
