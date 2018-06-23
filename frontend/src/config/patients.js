678294b6431fa924aa194f1f92853d0ff0aaaa55
Donald Wu <yeukfei02@gmail.com>
2018-06-23 15:46:16 +0800

    add more dummy data

--- frontend/src/config/patients.js
+++ frontend/src/config/patients.js
@@ -4,5 +4,23 @@ export default {
     name: "Stanley Gunawan",
     age: 31,
     imageUrl: "/images/8780191.jpeg",
-  }
+  },
+  "111": {
+    id: '111',
+    name: "Donald Wu",
+    age: 27,
+    imageUrl: "",
+  },
+  "222": {
+    id: '222',
+    name: "Dummy Data",
+    age: 55,
+    imageUrl: "",
+  },
+  "333": {
+    id: '333',
+    name: "Dummy Data",
+    age: 60,
+    imageUrl: "",
+  },
 }
